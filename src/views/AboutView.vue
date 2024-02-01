5b5bc454fed0731b9bc692ca32a65a79a3587d22
jimkao <Jim87as0821@gmail.com>
2024-02-02 01:12:21 +0800

    vite環境測試

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data1 }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data1: {}
+    }
+  },
+  mounted() {
+    this.$http.get('https://randomuser.me/api/').then((response) => {
+      console.log(response.data)
+      this.data1 = response.data.results[0]
+    })
+  }
+}
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
